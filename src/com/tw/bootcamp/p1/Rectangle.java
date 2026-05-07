c2fc327647f0ce951adbc01e63bc640557c56100
adhi <adithyanadhi3118@gmail.com>
2026-05-07 11:53:00 +0530

    adds perimeter method in Rectangle class

--- src/com/tw/bootcamp/p1/Rectangle.java
+++ src/com/tw/bootcamp/p1/Rectangle.java
@@ -2,15 +2,25 @@ package com.tw.bootcamp.p1;
 
 public class Rectangle {
 
-  private final int length;
-  private final int breadth;
+  private final double length;
+  private final double breadth;
 
-  public Rectangle(int length, int breadth) {
+  private Rectangle(double length, double breadth) {
     this.length = length;
     this.breadth = breadth;
   }
 
-  public int area() {
+  public static Rectangle create(int length, int breadth) {
+
+    return new Rectangle(length, breadth);
+  }
+
+  public double area() {
     return length * breadth;
+  }
+
+
+  public double perimeter() {
+    return 2 * (length + breadth);
   }
 }
--- test/com/tw/bootcamp/p1/RectangleTest.java
+++ test/com/tw/bootcamp/p1/RectangleTest.java
@@ -2,22 +2,32 @@ package com.tw.bootcamp.p1;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RectangleTest {
   @Test
   public void shouldCalculateAreaFor10By5Rectangle() {
-    Rectangle rectangle = new Rectangle(10,5);
-    int area = rectangle.area();
+    Rectangle rectangle = Rectangle.create(10, 5);
+    double area = rectangle.area();
 
     assertEquals(50, area);
   }
 
   @Test
   void shouldReturnAreaOf1By1() {
-    Rectangle rectangle = new Rectangle(1, 1);
+    Rectangle rectangle = Rectangle.create(1, 1);
     assertEquals(1, rectangle.area());
   }
 
+  @Test
+  void shouldReturnAPerimeterOf24() {
+    Rectangle rectangle = Rectangle.create(4, 4);
+    assertEquals(16, rectangle.perimeter());
+  }
 
+  @Test
+  void shouldReturnAPerimeterOf4OfaRectOf1By1() {
+    Rectangle rectangle = Rectangle.create(1, 1);
+    assertEquals(4, rectangle.perimeter());
+  }
 }
